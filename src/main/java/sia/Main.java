3f338bca9c12bfe94a505f404bbbfefa6eabe88b
Your Name <you@example.com>
2025-09-28 00:13:27 -0300

    "Commit Final"

--- src/main/java/sia/Main.java
+++ src/main/java/sia/Main.java
@@ -1,21 +1,11 @@
 package sia;
 
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
-        Sistema sistema = new Sistema();
-        Menu menu = new Menu(sistema);
-
-        menu.mostrarMenu();
-
-        // guardar sistema cuando el usuario salga
-        try {
-            sistema.guardarSistema();
-            System.out.println("Sistema guardado correctamente al salir.");
-        } 
-        catch (Exception e) {
-            System.err.println("Error al guardar el sistema: " + e.getMessage());
-        }
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new MainWindow().setVisible(true);
+            }
+        });
     }
 }
